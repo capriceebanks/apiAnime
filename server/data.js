8f273f8bdfb6d6f109f69dc8afab22aec0ee0dbd
bojin11 <15011278723@163.com>
2022-01-21 14:24:34 +0000

    add pages

---
+++ server/data.js
@@ -0,0 +1,21 @@
+// My animes resource
+
+const animes = [
+
+{ id: 1, name:'bleach', picture:'https://m.media-amazon.com/images/M/MV5BZjE0YjVjODQtZGY2NS00MDcyLThhMDAtZGQwMTZiOWNmNjRiXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg'},
+{ id: 2, name:'black_clover', picture:'https://m.media-amazon.com/images/M/MV5BNTAzYTlkMWEtOTNjZC00ZDU0LWI5ODUtYTRmYzY0MTAzYWZlXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg'},
+{ id: 3, name:'dragon_ball', picture:'https://m.media-amazon.com/images/M/MV5BMGMyOThiMGUtYmFmZi00YWM0LWJiM2QtZGMwM2Q2ODE4MzhhXkEyXkFqcGdeQXVyMjc2Nzg5OTQ@._V1_FMjpg_UX1000_.jpg'},
+{ id: 4, name: 'jujutsu_kaisen', picture:'https://m.media-amazon.com/images/M/MV5BNzQyYzU3Y2MtOWY2Yy00ZGM2LTg3ZTUtMDJkZTJiMmEzMjYxXkEyXkFqcGdeQXVyMTI2NTY3NDg5._V1_.jpg'},
+{ id: 5, name:'fma_brotherhood', picture:'https://m.media-amazon.com/images/M/MV5BZmEzN2YzOTItMDI5MS00MGU4LWI1NWQtOTg5ZThhNGQwYTEzXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'},
+{ id: 6, name:'naruto', picture:'https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'},
+{ id: 7, name:'gintama', picture:'https://m.media-amazon.com/images/M/MV5BMDkxZTJjZTEtMDRjMy00Yzk1LWI5YjItMjIwYmVlYzhlZWZhXkEyXkFqcGdeQXVyNDQxNjcxNQ@@._V1_FMjpg_UX1000_.jpg'},
+{ id: 8, name:'itachi_uchiha', picture:'https://comicvine.gamespot.com/a/uploads/scale_small/0/378/859934-god2vj0xj3.jpg'},
+{ id: 9, name:'one_piece', picture:'https://m.media-amazon.com/images/M/MV5BODcwNWE3OTMtMDc3MS00NDFjLWE1OTAtNDU3NjgxODMxY2UyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg'},
+{ id: 10, name:'demon_slayer', picture:'https://m.media-amazon.com/images/M/MV5BZjZjNzI5MDctY2Y4YS00NmM4LTljMmItZTFkOTExNGI3ODRhXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_.jpg'},
+{ id: 11, name:'attack_on_titan', picture:'https://flxt.tmsimg.com/assets/p10701949_b_v8_ah.jpg'},
+{ id: 12, name:'hunter_x_hunter', picture:'https://m.media-amazon.com/images/M/MV5BZjNmZDhkN2QtNDYyZC00YzJmLTg0ODUtN2FjNjhhMzE3ZmUxXkEyXkFqcGdeQXVyNjc2NjA5MTU@._V1_FMjpg_UX1000_.jpg'},
+{ id: 13, name:'boku_no_hero_academia', picture:'https://i.pinimg.com/736x/0f/7f/ee/0f7feeb4655ffc029d1b9823bafb2ff8.jpg'},
+
+];
+
+module.exports = animes;
